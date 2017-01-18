b4bbe6035adc5ea7ba707b85af103ba6f8e8e6d6
Nicolas Limage <github@xephon.org>
2017-01-18 10:24:16 +0000

    improve reliability of travis script

--- travis.sh
+++ travis.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 DOCKERIMAGE="${KEXTRACT_DOCKERIMAGE:-kextract}"
-
 docker build -t "${DOCKERIMAGE}" .
 
 for dist in xenial yakkety
 do
   for arch in amd64 arm64 armhf i386 powerpc ppc64el
   do
-    docker run --rm -ti -v $(pwd)/dist:/workdir kextract $arch $dist
+    docker run --rm -ti -v "$(pwd)/dist:/workdir" "${DOCKERIMAGE}" "$arch" "$dist"
   done
 done
